443367752598efa23ff89b9e1f71bf9e8410453f
Jack Polk <jackson.polk-1@ou.edu>
2020-03-23 15:51:10 -0500

    lab 9 myboot2 function upload

---
+++ R/myboot2.R
@@ -0,0 +1,47 @@
+#' My function to create bootstrap samples
+#'
+#' Produces barplots and sample statistics based on parameters given
+#'
+#' This is a part of the lab 9 function requirement
+#'
+#' @param iter, a number of iterations
+#' @param x, a sample variable
+#' @param fun, a fucntion to apply to the sample
+#' @param alpha, confidence
+#' @param cx, size of abrs
+#' @param ...
+#'
+#' @return a barplot displaying the test statistic and the confidence interval
+#' @export
+#'
+#' @examples
+#' myboot2(x = c(1:10), fun = "median", alpha = 0.2)
+myboot2<-function(iter=10000,x,fun="mean",alpha=0.05,cx=1.5,...){  #Notice where the ... is repeated in the code
+  n=length(x)   #sample size
+
+  y=sample(x,n*iter,replace=TRUE)
+  rs.mat=matrix(y,nr=n,nc=iter,byrow=TRUE)
+  xstat=apply(rs.mat,2,fun) # xstat is a vector and will have iter values in it
+  ci=quantile(xstat,c(alpha/2,1-alpha/2))# Nice way to form a confidence interval
+  # A histogram follows
+  # The object para will contain the parameters used to make the histogram
+  para=hist(xstat,freq=FALSE,las=1,
+            main=paste("Histogram of Bootstrap sample statistics","\n","alpha=",alpha," iter=",iter,sep=""),
+            ...)
+
+  #mat will be a matrix that contains the data, this is done so that I can use apply()
+  mat=matrix(x,nr=length(x),nc=1,byrow=TRUE)
+
+  #pte is the point estimate
+  #This uses whatever fun is
+  pte=apply(mat,2,fun)
+  abline(v=pte,lwd=3,col="Black")# Vertical line
+  segments(ci[1],0,ci[2],0,lwd=4)      #Make the segment for the ci
+  text(ci[1],0,paste("(",round(ci[1],2),sep=""),col="Red",cex=cx)
+  text(ci[2],0,paste(round(ci[2],2),")",sep=""),col="Red",cex=cx)
+
+  # plot the point estimate 1/2 way up the density
+  text(pte,max(para$density)/2,round(pte,2),cex=cx)
+
+  invisible(list(ci=ci,fun=fun,x=x))# Some output to use if necessary
+}
